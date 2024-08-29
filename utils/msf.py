2f1d9a68e3d51a70937bb988c8d0cc7c03181ba8
psycore8 <76014584+psycore8@users.noreply.github.com>
2024-08-29 22:27:40 +0200

    Update msf.py

--- utils/msf.py
+++ utils/msf.py
@@ -9,4 +9,10 @@ class msfvenom:
     file_name = str(h.GenerateFileName())
     subprocess.run([msfvenom_path, "-p", payload, arg1, arg2, "-e", "generic/none", "--format", "raw", "-o", file_name])
     return file_name
-   
+  
+  def CreateShellcodeEx(msfvenom_path, msf_command):
+    msf_args = msf_command.split()
+    msf_args.insert(0, msfvenom_path)
+    print(f'Argument List: {msf_args}')
+    subprocess.run(msf_args)
+    return True
